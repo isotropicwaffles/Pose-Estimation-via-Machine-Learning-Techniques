0048f726a90a9fd25f7e3042821e876c1b759677
Alex <showri01@gmail.com>
2016-12-09 21:57:26 -0500

    Saving file.

--- CNN/run_cnn_analysis_Project.py
+++ CNN/run_cnn_analysis_Project.py
@@ -17,13 +17,13 @@ def run_all_cnn_analysis():
 
   
   # Hyperparameters arrays
-  num_hidden_layers_array =[3]
-  num_conv_layers_array =[2]
-  batch_size_array = [100]
-  learning_rate_array = [.1]
+  num_hidden_layers_array =[1]
+  num_conv_layers_array =[3]
+  batch_size_array = [10]
+  learning_rate_array = [.00001]
   conv_layer1_filter_size_array = [5] 
   conv_layer1_depth_array = [16]
-  conv_layer1_stride_array = [2] 
+  conv_layer1_stride_array = [1] 
   conv_layer2_filter_size_array = [5] 
   conv_layer2_depth_array = [16]
   conv_layer2_stride_array = [2] 
@@ -38,7 +38,7 @@ def run_all_cnn_analysis():
   num_training_steps_array = [5000] 
   
   # Add max pooling arrays
-  pooling_array = [False]
+  pooling_array = [True]
   conv_layer1_pool_filter_size_array = [2]
   conv_layer1_pool_stride_array = [2]
   conv_layer2_pool_filter_size_array = [2]
